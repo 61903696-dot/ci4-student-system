ae431aed35f6d63c199ef20a30d87a7c565a0aad
Asilod Ralph <Asilod@example.com>
2026-04-30 15:18:41 +0800

    Completed Student 1: Full Working CRUD with DB connection

--- app/Controllers/Student.php
+++ app/Controllers/Student.php
@@ -3,11 +3,10 @@
 namespace App\Controllers;
 
 use App\Models\StudentModel;
-use CodeIgniter\Controller;
 
-class Student extends Controller
+
+class Student extends BaseController
 {
-    // Show all students
     public function index()
     {
         $model = new StudentModel();
@@ -15,24 +14,21 @@ class Student extends Controller
         return view('student_view', $data);
     }
 
-    // Add new student
     public function store()
     {
         $model = new StudentModel();
-        $data = [
+        $model->save([
             'name'   => $this->request->getPost('name'),
             'email'  => $this->request->getPost('email'),
             'course' => $this->request->getPost('course'),
-        ];
-        $model->save($data);
-        return redirect()->to(base_url('student'));
+        ]);
+        return redirect()->to('/student');
     }
 
-    // Delete student
     public function delete($id = null)
     {
         $model = new StudentModel();
         $model->delete($id);
-        return redirect()->to(base_url('student'));
+        return redirect()->to('/student');
     }
 }
